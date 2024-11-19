44a0b2c603406e3f9ed170e7bbeb61f006c7810e
Jose Walter <jwdutra@gmail.com>
2024-11-19 17:53:23 -0300

    Module interceptor

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/ConstantsExample.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/ConstantsExample.kt
@@ -4,4 +4,9 @@ object ConstantsExample {
     const val  BASE_URL = "https://gateway.marvel.com/v1/public"
     const val PUBLIC_KEY = ""
     const val PRIVATE_KEY = ""
+
+    const val TS = "1"
+    const val HASH = "hash"
+    const val APIKEY = "apikey"
+
 }
