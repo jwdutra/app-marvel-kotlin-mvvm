c6567506748038361d0d77b170383d919628ab88
Jose Walter <jwdutra@gmail.com>
2024-11-19 15:23:35 -0300

    Service remote configuration

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/ConstantsExample.kt
@@ -0,0 +1,7 @@
+package daniel.lop.io.marvelappstarter.util
+
+object ConstantsExample {
+    const val  BASE_URL = "https://gateway.marvel.com/v1/public"
+    const val PUBLIC_KEY = ""
+    const val PRIVATE_KEY = ""
+}
