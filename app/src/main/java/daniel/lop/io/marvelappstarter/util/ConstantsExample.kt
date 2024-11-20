a99fb0b71c373757903ab7ae1a4cdf4b2578645e
Jose Walter <jwdutra@gmail.com>
2024-11-20 15:56:23 -0300

    Search viewModel implementation

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/ConstantsExample.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/ConstantsExample.kt
@@ -1,11 +1,11 @@
 package daniel.lop.io.marvelappstarter.util
 
 object ConstantsExample {
-    const val  BASE_URL = "https://gateway.marvel.com/v1/public"
+    const val  BASE_URL = "https://gateway.marvel.com/v1/public/"
     const val PUBLIC_KEY = ""
     const val PRIVATE_KEY = ""
 
-    const val TS = "1"
+    const val TS = "ts"
     const val HASH = "hash"
     const val APIKEY = "apikey"
 
