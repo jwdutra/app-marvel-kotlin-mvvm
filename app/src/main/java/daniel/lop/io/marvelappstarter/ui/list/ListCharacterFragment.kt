9695f7c7c56820e78699e464a4fcff38196a55d0
Jose Walter <jwdutra@gmail.com>
2024-11-20 15:31:42 -0300

    List Characters implementation

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/list/ListCharacterFragment.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/list/ListCharacterFragment.kt
@@ -1,25 +1,83 @@
 package daniel.lop.io.marvelappstarter.ui.list
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
 import daniel.lop.io.marvelappstarter.databinding.FragmentListCharacterBinding
+import daniel.lop.io.marvelappstarter.ui.adapters.CharacterAdapter
 import daniel.lop.io.marvelappstarter.ui.base.BaseFragment
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import daniel.lop.io.marvelappstarter.util.hide
+import daniel.lop.io.marvelappstarter.util.show
+import daniel.lop.io.marvelappstarter.util.toast
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import kotlinx.coroutines.flow.collect
 
-// Esta classe representa um fragmento na aplicação Android que exibe uma lista de personagens.
-// Ela estende a classe BaseFragment, que é uma classe genérica que recebe dois parâmetros de tipo:
-// FragmentListCharacterBinding e ListCharacterViewModel.
 @AndroidEntryPoint
 class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCharacterViewModel>() {
 
-    // A propriedade viewModel é inicializada usando o delegate viewModels, que fornece um ViewModel com escopo para este Fragment.
     override val viewModel: ListCharacterViewModel by viewModels()
+    private val characterAdapter by lazy { CharacterAdapter() }
 
-    // Este método é responsável por inflar o layout para este fragmento.
-    // Ele usa a classe FragmentListCharacterBinding para inflar o layout e retornar o objeto de binding.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        clickAdapter()
+        collectObserver()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch  {
+        viewModel.list.collect { resource ->
+            when (resource) {
+                is ResourceState.Success -> {
+                    resource.data?.let { values ->
+                        binding.progressCircular.hide()
+                        characterAdapter.characters = values.data.results.toList()
+                    }
+                }
+                is ResourceState.Error -> {
+                    binding.progressCircular.hide()
+                    resource.message?.let { message ->
+                        toast(getString(R.string.an_error_occurred))
+                        Timber.tag("ListCharacterFragment").e("Error -> $message")
+                    }
+                }
+                is ResourceState.Loading -> {
+                    binding.progressCircular.show()
+
+                }
+                else -> {}
+
+            }
+        }
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        rvCharacters.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnItemClickListener { characterModel ->
+            val action = ListCharacterFragmentDirections
+                .actionListCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+        }
+    }
+
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentListCharacterBinding = FragmentListCharacterBinding.inflate(inflater, container, false)
 }
+
